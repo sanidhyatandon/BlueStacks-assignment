e4dd3214a03dd628186e5fe4b2f4f04472763db7
Sanidhya Tandon <sanidhya_tandon@crgl-thirdparty.com>
2021-04-01 13:26:38 +0530

    Code Update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Suspense } from 'react';
+import React, { Component } from 'react';
 
 // import route Components here
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -6,17 +6,12 @@ import routes from './routes/routing';
 import { createBrowserHistory } from 'history';
 import './i18n';
 import './common/utilities.scss';
-import Loader from './common/Loader';
 
 const history = createBrowserHistory();
 
 class App extends Component {
   render() {
-    return (
-      <Suspense fallback={<Loader />}>
-        <Router history={history}>{routes}</Router>
-      </Suspense>
-    );
+    return <Router history={history}>{routes}</Router>;
   }
 }
 export default App;
